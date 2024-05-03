3cbe375b405514f96d2ee73421ce01a8e62ecbd2
CatLan04 <chantran29102004@gmail.com>
2024-05-03 16:26:40 +0700

    fe

---
+++ fe/productcard.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Các đoạn mã JavaScript của bạn ở đây
+    const imgs = document.querySelectorAll('.img-select a');
+    const imgBtns = [...imgs];
+    let imgId = 1;
+
+    imgBtns.forEach((imgItem) => {
+        imgItem.addEventListener('click', (event) => {
+            event.preventDefault();
+            imgId = imgItem.dataset.id;
+            slideImage(); // Gọi slideImage() sau khi imgId đã được cập nhật
+        });
+    });
+
+    function slideImage(){
+        const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+        document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+    }
+
+    window.addEventListener('resize', slideImage);
+});
+$(".qtyminus").on("click",function(){
+    var now = $(".qty").val();
+    if ($.isNumeric(now) && parseInt(now) > 1) {
+        $(".qty").val(parseInt(now) - 1);
+    }
+});
+
+$(".qtyplus").on("click",function(){
+    var now = $(".qty").val();
+    if ($.isNumeric(now)) {
+        $(".qty").val(parseInt(now) + 1);
+    }
+});
+
+
